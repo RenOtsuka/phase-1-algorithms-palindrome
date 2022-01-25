e45bc90fde31490c4ac9ba09327cbce04e0843e1
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-01-25 10:42:13 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let i = 0; 
+  //console.log(word.length/2);
+  for(; i < word.length / 2; i++){
+    if(word[i] !== word[word.length - 1 - i]){
+      return false;
+    }
+    else if(word[i] === word[word.length - 1 - i]){
+      continue;
+    }
+  }
+
+  if(i === Math.ceil(word.length / 2)){
+    return true;
+  }
 }
 
 /* 
   Add your pseudocode here
+
+  Initialize for loop and pass the word into it
+      Checks if the starting letter is not equal to the last letter
+      If true, return false and breaks loop
+      If false, continue the loop until it reaches the halfway point of the word
+  If the index variable has reached the halfway point of the word w/o return false,
+  then it returns true
+
 */
 
 /*
   Add written explanation of your solution here
+  We only need to check up to halfway of the word to check for palindrome
+  I used a for loop because we know exactly how many times we need iterate through each letter
+  I round up the index of the halfway point of the word for odd number words since the index variable
+  is counting as an integer
+
 */
 
 // You can run `node index.js` to view these console logs
